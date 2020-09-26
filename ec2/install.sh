6979955763a5621f8cf79d14ace52a151fc7ca41
andreich78-20 <68030111+andreich78-20@users.noreply.github.com>
2020-09-26 17:48:55 +0300

    Fixed lost "-y" switch at localinstall

--- ec2/install.sh
+++ ec2/install.sh
@@ -2,10 +2,9 @@
 
 sudo -E bash -
 sudo yum update -y && sudo yum install -y git
-git clone https://github.com/andreich78-20/akvelon-cloud-aws.git akvelon-db && \
-  cd akvelon-db/ec2/app
-sudo wget https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
-sudo yum localinstall mysql57-community-release-el7-11.noarch.rpm 
-sudo yum install -y mysql-community-server
-sudo chkconfig mysqld on
+git clone https://github.com/andreich78-20/akvelon-cloud-aws.git akvelon-db
+sudo wget https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm && \
+sudo yum localinstall -y mysql57-community-release-el7-11.noarch.rpm && \
+sudo yum install -y mysql-community-server && \
+sudo chkconfig mysqld on && \
 sudo service mysqld start
