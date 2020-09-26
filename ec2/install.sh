93b071d4804efdcacbf317eddd30ddd3f572e851
andreich78-20 <68030111+andreich78-20@users.noreply.github.com>
2020-09-26 15:26:47 +0300

    Change repo address to point to my branch

--- ec2/install.sh
+++ ec2/install.sh
@@ -2,7 +2,7 @@
 
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get update && sudo apt-get install -y nodejs npm git
-git clone https://github.com/ega-forever/akvelon-cloud-aws.git akvelon && \
+git clone https://github.com/andreich78-20/akvelon-cloud-aws.git akvelon && \
   cd akvelon/ec2/app && npm install && \
   npm install -g pm2 && pm2 start index.js && \
   pm2 startup systemd && pm2 save
