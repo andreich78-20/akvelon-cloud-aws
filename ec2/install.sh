6392a29a7abbfa4076c85660b757d704d83c5831
andreich78-20 <68030111+andreich78-20@users.noreply.github.com>
2020-09-27 18:15:58 +0300

    Refactored init script

--- ec2/install.sh
+++ ec2/install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
-sudo yum update && sudo yum install -y nodejs npm git
-sudo git clone https://github.com/andreich78-20/akvelon-cloud-aws.git akvelon-web-server && \
-  cd akvelon-web-server/ec2/app && sudo npm install && \
-  sudo npm install -g pm2 mysql && sudo pm2 start index.js && \
-  sudo pm2 startup systemd && sudo pm2 save
+yum update && yum install -y nodejs npm git
+git clone https://github.com/andreich78-20/akvelon-cloud-aws.git akvelon-web-server
+cd akvelon-web-server/ec2/app
+npm install
+npm install -y mysql
+npm install -g pm2
+pm2 start index.js
+pm2 startup systemd
+pm2 save
