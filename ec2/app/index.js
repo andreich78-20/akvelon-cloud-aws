20db6891823cbc559c5e2133a6713b189f278d12
andreich78-20 <68030111+andreich78-20@users.noreply.github.com>
2020-09-27 12:48:19 +0300

    works

--- ec2/app/index.js
+++ ec2/app/index.js
@@ -1,14 +1,56 @@
+const mysql = require('mysql');
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ok: 1, pid: process.pid});
+var mysqlConnection = mysql.createConnection({
+host: '18.237.56.171',
+user: 'dbuser',
+password: 'akvelon#cLoudUser2020',
+database: 'cloud_test',
+multipleStatements: true
 });
 
+mysqlConnection.connect((err)=> {
+if(!err)
+  console.log('Connection established successfully.');
+else
+  console.log('Connection failed!'+ JSON.stringify(err,undefined,2));
+});
+
+  mysqlConnection.query("CALL GetIncrementedVisitorsCounter;", [], (err, rows, fields) => {
+    if (!err) {
+      rows.forEach( (element) => {
+        if(element.constructor == Array)
+          console.log(element[0].IncrementedCounter);
+        else
+          console.log(element);
+      });
+    }
+    else {
+      console.log(err);
+    }
+  });
+
+app.get('/', (req, res) => {
+  mysqlConnection.query("CALL GetIncrementedVisitorsCounter;", [], (err, rows, fields) => {
+    if (!err)
+      rows.forEach( (element) => {
+        if(element.constructor == Array)
+          res.send(element[0].IncrementedCounter);
+        else
+          res.send(element);
+      });
+    else {
+      res.status(500);
+      res.send(err);
+    }
+  });
+  // res.send({ok: 1, pid: process.pid})
+});
 
 app.get('/long', (req, res) => {
   setTimeout(() => {
-    res.send({ok: 2, pid: process.pid});
+    res.send({ok: 1, pid: process.pid})
   }, 5000);
 });
 
